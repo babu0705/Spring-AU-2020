052f45d4c10d705471382d16b0e0b6313e2b664f
babu0705 <59680254+babu0705@users.noreply.github.com>
2020-01-26 14:33:52 +0530

    REST API

---
+++ 21-01-2020-AN/demo1/src/main/java/com/example/demo/User.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class User {
+	public String username;
+	public String password;
+ 
+public User(String username,String pass) {
+	 this.username=username;
+	 this.password=pass;
+}
+
+ 
+}
