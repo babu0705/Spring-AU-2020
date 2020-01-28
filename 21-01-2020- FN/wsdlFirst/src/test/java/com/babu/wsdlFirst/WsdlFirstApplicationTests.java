cb3ce35b0cc085f3cc84d47a41fa2d2051647710
babu0705 <59680254+babu0705@users.noreply.github.com>
2020-01-28 23:13:41 +0530

    SOAP API
    Output is shared as a screenshot

---
+++ 21-01-2020- FN/wsdlFirst/src/test/java/com/babu/wsdlFirst/WsdlFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babu.wsdlFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
